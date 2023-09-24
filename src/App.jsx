0f6e2b42624fe9c2915459473ddfa8d9931d16e2
Naeem Hossen <132334814+NHossen@users.noreply.github.com>
2023-09-24 21:26:27 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+
+  return (
+
+   <div>
+
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn btn-accent">Accent</button>
+   </div>
+
+  )
+}
+
+export default App
